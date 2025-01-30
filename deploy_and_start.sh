8195f88f50c9efb7c2c437ce419ac68bcf50c3df
mouadh15 <95212443+mouadh15@users.noreply.github.com>
2025-01-30 18:10:43 +0100

    Update deploy_and_start.sh

--- deploy_and_start.sh
+++ deploy_and_start.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-
-echo "Mise à jour des paquets"
-sudo apt update -y
-
-# Installation de Docker
-echo "Installation  Docker"
-sudo apt install \
-apt-transport-https \
-ca-certificates \
-curl \
-software-properties-common
-
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -20,17 +7,12 @@ echo \
 "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt update
-
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Vérification de l'installation de Docker
 echo "Vérification de l'installation de Docker"
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo docker --version
-
 
 # Installation de Ansible
 echo "Installation de Ansible"
@@ -38,22 +20,6 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
-# Vérification de l'installation d'Ansible
-echo "Vérification Ansible"
-ansible --version
-
-# Installation de Tree
-echo "Installation Tree"
-sudo apt install -y tree
-
-# Vérification de l'installation de Tree
-echo "Vérification de l'installation de Tree"
-tree --version
-
-
-
-# Installation de Node Exporter
-echo "Installation de Node Exporter"
 
 # Télécharger la dernière version de Node Exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-amd64.tar.gz
@@ -86,13 +52,6 @@ sudo systemctl daemon-reload
 echo "Démarrage de Node Exporter"
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
-
-# Vérifier si Node Exporter fonctionne
-echo "Installation terminée !"
-
-
-# Lancement des playbooks
-echo "Lancement des playbooks"
 
 ansible-playbook playbooks/network_config.yml
 ansible-playbook -i inventaire.ini playbooks/conteneur_config.yml
